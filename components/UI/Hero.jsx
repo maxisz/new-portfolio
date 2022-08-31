11ac9c7985781fa381a76ddd458f58f5b6b5ef65
maxisz <maxwellkibetz@gmail.com>
2022-08-31 20:54:06 +0300

    my first commit

---
+++ components/UI/Hero.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import SectionSubtitle from "./SectionSubtitle";
+import { Container, Row, Col } from "reactstrap";
+import Image from "next/image";
+import Link from "next/link";
+// import heroImg from "../../public/images/hero.jpg";
+import heroImg from "../../public/Assets/img12.png";
+import classes from "../../styles/hero.module.css";
+
+const Hero = () => {
+  return (
+    <section className={`${classes.hero}`}>
+      <Container>
+        <Row>
+          {/* ========== hero content ============= */}
+          <Col lg="6" md="6">
+            <div className={`${classes.hero__content}`}>
+              <SectionSubtitle subtitle="Hello" />
+              <h2 className="mt-3 mb-3">I&apos;m Kemboi Maxwell</h2>
+              <h5 className="mb-4">Fullstack Developer</h5>
+              <p>
+               
+                I am a full stack developer with a passion for programming. 
+                I have a strong understanding of both front-end and back-end development.
+                I am also proficient in a variety of programming languages, including Java, php ,C , JavaScript, flutter 
+                and Python. I am a quick learner and have a keen eye for detail. 
+                I am also a strong communicator and have a proven track record of 
+                working well in team environments.
+              </p>
+              <div className="mt-5">
+                <button className="primary__btn">
+                  <Link href="#">Hire me</Link>
+                </button>
+
+                <button className="secondary__btn">
+                  <Link href="#">Download CV</Link>
+                </button>
+              </div>
+            </div>
+          </Col>
+
+          {/* ========== hero img ============= */}
+          <Col lg="6" md="6">
+            <div className={`${classes.hero__img} text-end`}>
+              <Image alt="hero-image" src={heroImg} width="400" height="400" />
+
+              <div className={`${classes.hero__skills}`}>
+                <h6>Skills</h6>
+                <span>
+                  <i className="ri-bar-chart-line"></i>
+                </span>
+                <span>
+                  <i className="ri-bar-chart-line"></i>
+                </span>
+                <span>
+                  <i className="ri-bar-chart-line"></i>
+                </span>
+                <span>
+                  <i className="ri-bar-chart-line"></i>
+                </span>
+                <span>
+                  <i className="ri-bar-chart-line"></i>
+                </span>
+                <span>
+                  <i className="ri-bar-chart-line"></i>
+                </span>
+              </div>
+
+              <div
+                className={`${classes.hero__experience} d-flex align-items-center gap-3`}
+              >
+                <span>
+                  <i className="ri-lightbulb-flash-line"></i>
+                </span>
+                <div className="bg-transparent">
+                  <h6>Experience</h6>
+                  <h5 className="mb-0">3 Years</h5>
+                </div>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ components/UI/Services.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import SectionSubtitle from "./SectionSubtitle";
+import classes from "../../styles/services.module.css";
+import ServicesItem from "./ServicesItem";
+
+const Services = () => {
+  return (
+    <section id="services">
+      <Container>
+        <Row>
+          <Col lg="6" md="6">
+            <div className={`${classes.services__container}`}>
+              <div>
+                <ServicesItem title="App Development" icon="ri-apps-line" />
+
+                <ServicesItem title="Web Development" icon="ri-computer-line" />
+              </div>
+
+              <ServicesItem
+                title="Full-Stack Development"
+                icon="ri-code-s-slash-line"
+              />
+            </div>
+          </Col>
+
+          <Col lg="6" md="6" className={`${classes.service__title}`}>
+            <SectionSubtitle subtitle="What I do" />
+            <h3 className="mb-0 mt-4">Better Design,</h3>
+            <h3 className="mb-4">Better Experience</h3>
+            <p>
+            I work as a full stack developer, meaning that I work with both the front end and back end of web development. 
+  I am responsible for the overall design and development of web applications. I also feel comfortable working with a team of developers to c
+  reate dynamic and interactive Softwares. I use a variety of programming languages and web technologies to create 
+  robust and user-friendly web applications.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default Services;
